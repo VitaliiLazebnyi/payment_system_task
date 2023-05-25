586939263c3e917433d9c580e000cbb2da04ea0b
Vitalii Lazebnyi <vitalii_lazebnyi@gmail.com>
2023-05-25 20:42:44 +0300

    rename test properly

--- spec/tasks/remove_old_transactions_spec.rb
+++ spec/tasks/remove_old_transactions_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe 'rake import:users' do
+describe 'rake remove:old:transactions' do
   before do
     time = 1.hour.ago - 1.second
     create(:authorize, created_at: time, updated_at: time, user: create(:merchant))
