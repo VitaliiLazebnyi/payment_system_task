e2a3e8597cdd6202cac207408e4eaf3691e2a7ee
Vitalii Lazebnyi <vitalii_lazebnyi@gmail.com>
2023-05-25 09:52:59 +0300

    Seed with admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Admin.create!( name: 'Admin', description: 'admin', email: 'admin@email.com' )
