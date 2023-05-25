13395b37e415c93c64394ab7779ee78c05243452
Vitalii Lazebnyi <vitalii_lazebnyi@gmail.com>
2023-05-25 21:58:11 +0300

    Destroy Merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
+  end
+
+  def destroy
+    Merchant.find(params[:id]).destroy
+    redirect_to merchants_url
   end
 end
--- spec/controllers/merchants_controller_spec.rb
+++ spec/controllers/merchants_controller_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe MerchantsController do
-  describe 'GET #index' do
-    before do
-      create(:merchant)
-    end
+  before do
+    create(:merchant)
+  end
 
+  describe 'GET #index' do
     it 'assigns transactions variable' do
       get :index
       expect(assigns(:merchants)).to eq [Merchant.last]
@@ -14,6 +14,18 @@ RSpec.describe MerchantsController do
     it 'returns 200 /ok/ response code' do
       get :index
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys merchant' do
+      expect { delete :destroy, params: { id: Merchant.last.id } }
+        .to change(Merchant, :count).by(-1)
+    end
+
+    it 'redirects to merchant index page' do
+      delete :destroy, params: { id: Merchant.last.id }
+      expect(response).to redirect_to(merchants_path)
     end
   end
 end
