6a1f7be6369d900c70c1ccaafec7706793cdd427
Vitalii Lazebnyi <vitalii_lazebnyi@gmail.com>
2023-06-23 13:24:20 +0300

    login operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'transactions#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resources :merchants, only: %i[index edit update destroy]
   resources :transactions, only: :index
