cd23877087413aff83cf279b433b75ec3c76802e
Vitalii Lazebnyi <vitalii_lazebnyi@gmail.com>
2023-06-25 03:09:09 +0300

    capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,10 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'webdrivers'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -35,6 +39,28 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
+  config.before(:all, type: :feature) do
+    Capybara.server = :puma, { Silent: true }
+    Capybara.ignore_hidden_elements = true
+  end
+
+  Capybara.register_driver :selenium do |app|
+    options = ::Selenium::WebDriver::Chrome::Options.new
+
+    # options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1366,720')
+
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  Capybara.javascript_driver = :selenium
+
+  # js_driver = ENV['GUI'] == '1' ? :selenium_chrome : :selenium_chrome_headless
+  # Capybara.javascript_driver = js_driver
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{Rails.root}/spec/fixtures"
 
