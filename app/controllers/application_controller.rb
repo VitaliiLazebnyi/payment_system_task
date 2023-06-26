326269a3d2a5ce9ee91a7cacd269a3cd94f40b6a
Vitalii Lazebnyi <vitalii_lazebnyi@gmail.com>
2023-06-26 16:22:04 +0300

    Error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,26 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+
   def require_login
     return if current_user || request.path == login_path
 
-    redirect_to login_path, alert: t('sessions.login.require')
+    redirect_to login_path, flash: { error: t('sessions.create.require') }
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def generate_flash_with(success)
+    flash.clear
+
+    if success
+      flash[:success] = t("#{controller_name}.#{action_name}.success")
+    else
+      flash[:error] = t("#{controller_name}.#{action_name}.failed")
+    end
   end
 
   helper_method :current_user
